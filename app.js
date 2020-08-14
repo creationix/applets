81850f176c812c8da311df9ac53a79b5f035a75d
Tim Caswell <tim@creationix.com>
2020-08-14 10:26:53 -0500

    Draw a house

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ let width
 /** @type {number} Height of viewport in pixels */
 let height
 
-const ctx = canvas.getContext('webgl2', { antialias: true })
+const ctx = canvas.getContext('webgl2', {
+  antialias: true,
+  preserveDrawingBuffer: false
+})
 if (!ctx) throw new Error('Problem getting webgl2 context')
 const gl = ctx
 
@@ -72,7 +75,7 @@ function draw () {
     void main(void) {
       gl_Position = projection * view * vec4(coordinates.xyz, 1.0);
       int meta = int(coordinates.w);
-      textureCoord = vec3((meta >> 16) & 0xf, (meta >> 12) & 0xf, meta & 0xfff);
+      textureCoord = vec3((meta >> 15) & 0x1f, (meta >> 10) & 0x1f, meta & 0x3ff);
       int ni = meta >> 20;
       int ns = (ni & 1)*2-1;
       normal = (ni&4)==4 ? vec3(ns,0,0) : (ni&2)==2 ? vec3(0,ns,0) : vec3(0,0,ns);
@@ -108,7 +111,7 @@ function draw () {
     canvas.setAttribute('width', width)
     canvas.setAttribute('height', height)
     gl.viewport(0, 0, width, height)
-    gl.uniformMatrix4fv(projectionVar, false, perspective(M, 1, width / height, 0.1, 10))
+    gl.uniformMatrix4fv(projectionVar, false, perspective(M, 1, width / height, 0.1, 100))
   }
   window.onresize = resize
 
@@ -129,35 +132,46 @@ function draw () {
     // gl.bindTexture(gl.TEXTURE_2D_ARRAY, null)
   }
 
-  // 12 bits for texture index
+  // 10 bits for texture index
   const BARREL_TOP = 0
   const BARREL_SIDE = 1
   const BARREL_BOTTOM = 2
   const BARREL_OPEN = 3
   const BOOKSHELF = 4
   const OAK_PLANKS = 5
-
+  const OAK_LOG = 6
+  const OAK_END = 7
+  const STRIPPED_OAK_LOG = 8
+  const STRIPPED_OAK_END = 9
   const STONE = 10
   const GRASS_BLOCK_TOP = 11
   const GRASS_BLOCK_SIDE = 12
   const DIRT_BLOCK = 13
-
   const HAY_TOP = 14
   const HAY_SIDE = 15
-
   const GLASS = 16
-
   const FURNACE_BOTTOM = 17
   const FURNACE_SIDE = 18
   const FURNACE_FRONT = 19
   const FURNACE_FRONT_ON = 20
+  const MUSIC_BOX_TOP = 21
+  const MUSIC_BOX_SIDE = 22
+  const LOOM_TOP = 23
+  const LOOM_FRONT = 24
+  const LOOM_SIDE = 25
+  const COBBLESTONE = 26
+  const CRAFTING_TABLE_TOP = 27
+  const CRAFTING_TABLE_FRONT = 28
+  const CRAFTING_TABLE_SIDE = 29
+  const PUMPKIN_TOP = 30
+  const PUMPKIN_SIDE = 31
 
-  // 4 bits for texture x coordinate
-  // 4 bits for texture y coordinate
+  // 5 bits for texture x coordinate (needs 0-16 range)
+  // 5 bits for texture y coordinate (needs 0-16 range)
   const TL = (w, h) => 0
-  const BL = (w, h) => h << 12
-  const TR = (w, h) => w << 16
-  const BR = (w, h) => w << 16 | h << 12
+  const BL = (w, h) => h << 10
+  const TR = (w, h) => w << 15
+  const BR = (w, h) => w << 15 | h << 10
 
   // 3 bits for normal index
   const RIGHT = 0 << 20 // 1,0,0
@@ -212,18 +226,57 @@ function draw () {
   }
   // Define the geometry and store it in buffer objects
   const data = new Float32Array([
-    ...generateBlock(2, 0, 2, 1, 1, 1, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK),
-    ...generateBlock(3, 0, 2, 1, 1, 1, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK),
-    ...generateBlock(-2, -1, -2, 5, 1, 5, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE, GRASS_BLOCK_TOP, DIRT_BLOCK, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE),
-    ...generateBlock(-3, -2, -3, 7, 1, 7, STONE, STONE, STONE, STONE, STONE, STONE),
-    ...generateBlock(-3, -1, -3, 7, 4, 1, GLASS, GLASS, GLASS, GLASS, GLASS, GLASS),
-    ...generateBlock(-3, -1, -1, 1, 3, 3, HAY_SIDE, HAY_SIDE, HAY_TOP, HAY_TOP, HAY_SIDE, HAY_SIDE),
-    ...generateBlock(2, 0, 3, 1, 1, 1, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE, GRASS_BLOCK_TOP, DIRT_BLOCK, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE),
-    ...generateBlock(2, 1, 2, 1, 1, 1, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE, GRASS_BLOCK_TOP, DIRT_BLOCK, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE),
-    ...generateBlock(0, 0, 0, 1, 1, 1, FURNACE_SIDE, FURNACE_SIDE, FURNACE_BOTTOM, FURNACE_BOTTOM, FURNACE_FRONT, FURNACE_FRONT_ON),
-    ...generateBlock(1, 0, 0, 1, 1, 1, BARREL_SIDE, BARREL_SIDE, BARREL_TOP, BARREL_BOTTOM, BARREL_SIDE, BARREL_SIDE),
-    ...generateBlock(-1, 0, 0, 1, 1, 1, BARREL_SIDE, BARREL_SIDE, BARREL_OPEN, BARREL_BOTTOM, BARREL_SIDE, BARREL_SIDE),
-    ...generateBlock(0, 0, 1, 1, 3, 1, BOOKSHELF, BOOKSHELF, OAK_PLANKS, OAK_PLANKS, BOOKSHELF, BOOKSHELF)
+    ...generateBlock(0, 0, 0, 16, 1, 16, STONE, STONE, STONE, STONE, STONE, STONE),
+    ...generateBlock(0, 1, 0, 16, 1, 16, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK, DIRT_BLOCK),
+
+    ...generateBlock(0, 2, 0, 5, 1, 16, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE, GRASS_BLOCK_TOP, DIRT_BLOCK, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE),
+    ...generateBlock(14, 2, 0, 2, 1, 16, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE, GRASS_BLOCK_TOP, DIRT_BLOCK, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE),
+    ...generateBlock(5, 2, 0, 9, 1, 4, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE, GRASS_BLOCK_TOP, DIRT_BLOCK, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE),
+    ...generateBlock(5, 2, 11, 9, 1, 5, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE, GRASS_BLOCK_TOP, DIRT_BLOCK, GRASS_BLOCK_SIDE, GRASS_BLOCK_SIDE),
+
+    ...generateBlock(5, 2, 4, 9, 1, 7, COBBLESTONE, COBBLESTONE, COBBLESTONE, COBBLESTONE, COBBLESTONE, COBBLESTONE),
+    ...generateBlock(5, 3, 4, 1, 5, 1, OAK_LOG, OAK_LOG, OAK_END, OAK_END, OAK_LOG, OAK_LOG),
+    ...generateBlock(5, 3, 10, 1, 5, 1, OAK_LOG, OAK_LOG, OAK_END, OAK_END, OAK_LOG, OAK_LOG),
+    ...generateBlock(13, 3, 4, 1, 5, 1, OAK_LOG, OAK_LOG, OAK_END, OAK_END, OAK_LOG, OAK_LOG),
+    ...generateBlock(13, 3, 10, 1, 5, 1, OAK_LOG, OAK_LOG, OAK_END, OAK_END, OAK_LOG, OAK_LOG),
+
+    ...generateBlock(5, 3, 5, 1, 2, 5, STRIPPED_OAK_LOG, STRIPPED_OAK_LOG, STRIPPED_OAK_END, STRIPPED_OAK_END, STRIPPED_OAK_LOG, STRIPPED_OAK_LOG),
+    ...generateBlock(5, 6, 5, 1, 2, 5, STRIPPED_OAK_LOG, STRIPPED_OAK_LOG, STRIPPED_OAK_END, STRIPPED_OAK_END, STRIPPED_OAK_LOG, STRIPPED_OAK_LOG),
+    ...generateBlock(5, 8, 6, 1, 1, 3, STRIPPED_OAK_LOG, STRIPPED_OAK_LOG, STRIPPED_OAK_END, STRIPPED_OAK_END, STRIPPED_OAK_LOG, STRIPPED_OAK_LOG),
+    ...generateBlock(5, 9, 7, 1, 1, 1, STRIPPED_OAK_LOG, STRIPPED_OAK_LOG, STRIPPED_OAK_END, STRIPPED_OAK_END, STRIPPED_OAK_LOG, STRIPPED_OAK_LOG),
+    ...generateBlock(5, 5, 5, 1, 1, 5, GLASS, GLASS, GLASS, GLASS, GLASS, GLASS),
+
+    ...generateBlock(6, 3, 4, 1, 5, 1, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS),
+    ...generateBlock(12, 3, 4, 1, 5, 1, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS),
+    ...generateBlock(7, 3, 4, 5, 1, 1, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS),
+    ...generateBlock(7, 6, 4, 5, 2, 1, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS),
+    ...generateBlock(7, 4, 4, 5, 2, 1, GLASS, GLASS, GLASS, GLASS, GLASS, GLASS),
+
+    ...generateBlock(6, 3, 10, 1, 5, 1, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS),
+    ...generateBlock(12, 3, 10, 1, 5, 1, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS),
+    ...generateBlock(7, 3, 10, 5, 1, 1, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS),
+    ...generateBlock(7, 6, 10, 5, 2, 1, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS),
+    ...generateBlock(7, 4, 10, 5, 2, 1, GLASS, GLASS, GLASS, GLASS, GLASS, GLASS),
+    ...generateBlock(7, 6, 10, 5, 2, 1, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS, OAK_PLANKS),
+
+    ...generateBlock(1, 3, 2, 1, 1, 1, PUMPKIN_SIDE, PUMPKIN_SIDE, PUMPKIN_TOP, PUMPKIN_TOP, PUMPKIN_SIDE, PUMPKIN_SIDE),
+    ...generateBlock(2, 3, 4, 1, 1, 1, PUMPKIN_SIDE, PUMPKIN_SIDE, PUMPKIN_TOP, PUMPKIN_TOP, PUMPKIN_SIDE, PUMPKIN_SIDE),
+    ...generateBlock(3, 3, 3, 1, 1, 1, PUMPKIN_SIDE, PUMPKIN_SIDE, PUMPKIN_TOP, PUMPKIN_TOP, PUMPKIN_SIDE, PUMPKIN_SIDE),
+    ...generateBlock(2, 3, 2, 1, 1, 1, PUMPKIN_SIDE, PUMPKIN_SIDE, PUMPKIN_TOP, PUMPKIN_TOP, PUMPKIN_SIDE, PUMPKIN_SIDE),
+    ...generateBlock(4, 3, 1, 1, 1, 1, PUMPKIN_SIDE, PUMPKIN_SIDE, PUMPKIN_TOP, PUMPKIN_TOP, PUMPKIN_SIDE, PUMPKIN_SIDE),
+
+    ...generateBlock(6, 3, 5, 2, 2, 3, HAY_SIDE, HAY_SIDE, HAY_TOP, HAY_TOP, HAY_SIDE, HAY_SIDE),
+    ...generateBlock(8, 3, 5, 1, 1, 1, BARREL_SIDE, BARREL_SIDE, BARREL_TOP, BARREL_BOTTOM, BARREL_SIDE, BARREL_SIDE),
+    ...generateBlock(6, 3, 8, 1, 1, 1, BARREL_SIDE, BARREL_SIDE, BARREL_OPEN, BARREL_BOTTOM, BARREL_SIDE, BARREL_SIDE),
+
+    ...generateBlock(6, 3, 9, 2, 3, 1, BOOKSHELF, BOOKSHELF, OAK_PLANKS, OAK_PLANKS, BOOKSHELF, BOOKSHELF),
+    ...generateBlock(9, 3, 9, 1, 1, 1, FURNACE_SIDE, FURNACE_SIDE, FURNACE_BOTTOM, FURNACE_BOTTOM, FURNACE_SIDE, FURNACE_FRONT),
+    ...generateBlock(10, 3, 9, 1, 1, 1, FURNACE_SIDE, FURNACE_SIDE, FURNACE_BOTTOM, FURNACE_BOTTOM, FURNACE_SIDE, FURNACE_FRONT_ON),
+    ...generateBlock(11, 3, 9, 1, 1, 1, MUSIC_BOX_SIDE, MUSIC_BOX_SIDE, MUSIC_BOX_TOP, MUSIC_BOX_TOP, MUSIC_BOX_SIDE, MUSIC_BOX_SIDE),
+
+    ...generateBlock(11, 3, 5, 1, 1, 1, LOOM_SIDE, LOOM_SIDE, LOOM_TOP, OAK_PLANKS, LOOM_FRONT, LOOM_SIDE),
+    ...generateBlock(12, 3, 5, 1, 1, 1, CRAFTING_TABLE_SIDE, CRAFTING_TABLE_SIDE, CRAFTING_TABLE_TOP, OAK_PLANKS, CRAFTING_TABLE_FRONT, CRAFTING_TABLE_SIDE)
+
   ])
   const vertexBuffer = gl.createBuffer()
   if (!vertexBuffer) throw new Error('Problem creating buffer')
@@ -249,7 +302,7 @@ function draw () {
   gl.enable(gl.CULL_FACE)
   gl.cullFace(gl.BACK)
 
-  const center = [0.5, 0.5, 0.5]
+  const center = [8, 8, 8]
   const up = [0, 1, 0]
 
   resize()
@@ -267,8 +320,8 @@ function draw () {
   function render () {
     const nowx = anglex || Date.now() * 0.001
     const nowy = angley || Date.now() * 0.0003
-    const camera = [Math.sin(nowx) * 3, Math.sin(nowy) * 3, Math.cos(nowx) * 3]
-    targetTo(M, scaleAndAdd(camera, center, normalize(camera, camera), 5), center, up)
+    const camera = [Math.sin(nowx) * 16, Math.sin(nowy) * 16, Math.cos(nowx) * 16]
+    targetTo(M, scaleAndAdd(camera, center, normalize(camera, camera), 30), center, up)
     invert(M, M)
     gl.uniformMatrix4fv(viewVar, false, M)
 
@@ -277,7 +330,6 @@ function draw () {
 
     gl.drawArrays(gl.TRIANGLES, 0, data.length >> 2)
 
-    gl.flush()
     window.requestAnimationFrame(render)
   }
 }
